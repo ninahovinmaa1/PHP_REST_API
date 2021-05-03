7d54a1fba3b62db3d1a03f7ecb7fedbd7ed7c1d4
Nina <>
2021-05-03 07:42:25 +0200

    get products, convert to json, render to browser

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header("Referrer-Policy: no-referrer");
+header("Content-Type: application/json; charset=UTF-8");
+
+$allProducts = json_decode(file_get_contents("./products.json"), true);
+
+renderJson($allProducts);
+
+function renderJson($array) {
+    $json = json_encode($array, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+    echo $json; 
+}
